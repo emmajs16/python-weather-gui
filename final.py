30890de397ee86d54c5d94cee25af8a4cd894611
Emma Stoverink <emmastoverink@Emmas-MacBook-Pro-4.local>
2018-12-03 21:44:27 -0500

    updated comments

--- final.py
+++ final.py
@@ -1,16 +1,19 @@
+# Emma Stoverink
+# November 20, 2018
+# emmajs16@live.com
+# CSC 2280 LC1
+# Final Project
+# I will practice academic and personal integrity and excellence of character and expect the same from others.
+
+
 # GUI setup
 try:
-##     for Python2
+#     for Python2
   from Tkinter import *
 except ImportError:
     # for Python3
     from tkinter import *
-
-window = Tk()
-window.title("Weather Or Not")
-window.geometry('800x800')
-window.configure(background='#e5c3c6', cursor = "umbrella")
-
+    
 # API setup
 import json
 try:
@@ -19,13 +22,21 @@ try:
 except ImportError:
     # Fall back to Python 2's urllib2
     from urllib2 import urlopen
+    
+    
+# Set up window
+window = Tk()
+window.title("Weather Or Not")
+window.geometry('800x800')
+window.configure(background='#ffe4d5', cursor = "umbrella")
+
 
 # title label and label position
-title = Label(window, text="Weather Or Not", font=("BodoniSvtyTwoOSITCTT-Bold", 50),bg='#e5c3c6') #attributes include text and font, and font size
+title = Label(window, text="Weather Or Not", font=("BodoniSvtyTwoOSITCTT-Bold", 50),bg='#ffe4d5') #attributes include text and font, and font size
 title.grid(column=1, row=0, columnspan=1)
 
 #city label
-city_label = Label(window, text="City:", font=("BodoniSvtyTwoOSITCTT-Bold", 25),bg='#e5c3c6') #attributes include text and font, and font size
+city_label = Label(window, text="City:", font=("BodoniSvtyTwoOSITCTT-Bold", 25),bg='#ffe4d5') #attributes include text and font, and font size
 city_label.grid(column=0, row=2)
 
 # city input
@@ -34,7 +45,7 @@ city.grid(column=0, row=3)
 city.configure(font=("BodoniSvtyTwoOSITCTT-Book", 20))
 
 #state label
-state_label = Label(window, text="State:", font=("BodoniSvtyTwoOSITCTT-Bold", 25),bg='#e5c3c6') #attributes include text and font, and font size
+state_label = Label(window, text="State:", font=("BodoniSvtyTwoOSITCTT-Bold", 25),bg='#ffe4d5') #attributes include text and font, and font size
 state_label.grid(column=2, row=2)
 
 # state input
@@ -43,29 +54,29 @@ state.grid(column=2, row=3)
 state.configure(font=("BodoniSvtyTwoOSITCTT-Book", 20))
 
 # activity label
-activity_label = Label(window, text="What are your plans for today?", font=("BodoniSvtyTwoOSITCTT-Bold", 25),bg='#e5c3c6') #attributes include text and font, and font size
+activity_label = Label(window, text="What are your plans for today?", font=("BodoniSvtyTwoOSITCTT-Bold", 25),bg='#ffe4d5') #attributes include text and font, and font size
 activity_label.grid(column=1, row=4)
 # activity input
 selected_activity = IntVar()
-rad1 = Radiobutton(window,text='Relax', value=1, variable=selected_activity,bg='#e5c3c6')
-rad2 = Radiobutton(window,text="Exercise", value=2, variable=selected_activity,bg='#e5c3c6')
-rad3 = Radiobutton(window,text='School', value=3, variable=selected_activity,bg='#e5c3c6')
-rad4 = Radiobutton(window,text='Work', value=4, variable=selected_activity,bg='#e5c3c6')
+rad1 = Radiobutton(window,text='Relax', value=1, variable=selected_activity,bg='#ffe4d5')
+rad2 = Radiobutton(window,text="Exercise", value=2, variable=selected_activity,bg='#ffe4d5')
+rad3 = Radiobutton(window,text='School', value=3, variable=selected_activity,bg='#ffe4d5')
+rad4 = Radiobutton(window,text='Work', value=4, variable=selected_activity,bg='#ffe4d5')
 rad1.grid(column=0, row=5)
 rad2.grid(column=2, row=5)
 rad3.grid(column=0, row=6)
 rad4.grid(column=2, row=6)
 # temperature label
-temp_label = Label(window, font=("BodoniSvtyTwoOSITCTT-Bold", 25),bg='#e5c3c6')#attributes include text and font, and font size
+temp_label = Label(window, font=("BodoniSvtyTwoOSITCTT-Bold", 25),bg='#ffe4d5')#attributes include text and font, and font size
 temp_label.grid(column=0, row=8,columnspan=3)
 # interest suggestion label
-suggestion_label =  Label(window, font=("BodoniSvtyTwoOSITCTT-Bold", 25),bg='#e5c3c6')
+suggestion_label =  Label(window, font=("BodoniSvtyTwoOSITCTT-Bold", 25),bg='#ffe4d5')
 suggestion_label.grid(column=0, row=9,columnspan=3)
 # recommendation label
-recommendation_label =  Label(window, font=("BodoniSvtyTwoOSITCTT-Bold", 25),bg='#e5c3c6')
+recommendation_label =  Label(window, font=("BodoniSvtyTwoOSITCTT-Bold", 25),bg='#ffe4d5')
 recommendation_label.grid(column=0, row=10,columnspan=3)
 # Previously searched cities list
-previous_cities_label =  Label(window, font=("BodoniSvtyTwoOSITCTT-Bold", 25),bg='#e5c3c6',text= "Previously Searched Cities:")
+previous_cities_label =  Label(window, font=("BodoniSvtyTwoOSITCTT-Bold", 25),bg='#ffe4d5',text= "Previously Searched Cities:")
 previous_cities_label.grid(column=0, row=12,columnspan=3)
 
 
@@ -75,12 +86,17 @@ def clicked():
     # get the city and state search results from the user input field
     city_search = city.get()
     state_search = state.get()
+    # put search cities into the json url to get data from the Yahoo Weather API
     url = urlopen("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22{}%2C%20{}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys".format(city_search.replace(" ",""),state_search.replace(" ","")))
     result = dict(json.load(url))
+    # get the temperature as an integer
     temperature = int(result.get('query').get('results').get('channel').get('item').get('condition').get('temp'))
+    # get the condition data as a string
     condition = result.get('query').get('results').get('channel').get('item').get('condition').get('text')
+    
     temp_label.configure(text="It is {} degrees and {} in {}".format(temperature,condition.lower(),city.get().capitalize()))
     
+    # append the search city to the list of previously searched cities if it is not in the list yet
     if city_search not in previously_searched_cities:
         previously_searched_cities.append(city_search)
     
@@ -129,7 +145,7 @@ def clicked():
     # Display previously searched cities
     for i in range(0,len(previously_searched_cities)):
         city_text = previously_searched_cities[i].capitalize()
-        previous_cities_label =  Label(window, font=("BodoniSvtyTwoOSITCTT-Bold", 25),bg='#e5c3c6',text= city_text)
+        previous_cities_label =  Label(window, font=("BodoniSvtyTwoOSITCTT-Bold", 25),bg='#ffe4d5',text= city_text)
         previous_cities_label.grid(column=0, row=13+i,columnspan=3)
 
 
